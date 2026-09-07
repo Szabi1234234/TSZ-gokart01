a8c412194e1c8e96a4c1412248f9533d90cb420e
Szabi1234234 <toth.szabolcs1@diak.szbi-pg.hu>
2026-09-07 08:35:07 +0200

    Add project files.

---
+++ TSZ-gokart01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSZ_gokart01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
